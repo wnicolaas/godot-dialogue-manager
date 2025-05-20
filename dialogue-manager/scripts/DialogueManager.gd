e1f11a400b30f612bc610707a06544c90909b650
Wietse Nicolaas <wietsenicolaas@gmail.com>
2025-05-20 23:19:30 +0200

    (WIP) - Add inital setup dialogue manager

--- dialogue-manager/scripts/DialogueManager.gd
+++ dialogue-manager/scripts/DialogueManager.gd
@@ -1,10 +1,45 @@
-class_name DialogueManager extends Node
+extends Node2D
 
+var dialogue_path = "res://dialogues"
 
-# Called when the node enters the scene tree for the first time.
+@onready
+var DialogueSelector = $HUD/VBoxContainer/DialogueSelector;
+
 func _ready() -> void:
-	pass # Replace with function body.
+	refresh_ui()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func refresh_ui() -> void:
+	DialogueSelector.clear()
+	var directory_names = list_directories(dialogue_path)
+	
+	for directory in directory_names:
+		DialogueSelector.add_item(directory)
+	
+	DialogueSelector.select(-1)
+
+func list_directories(path: String) -> Array:
+	var dir := DirAccess.open(path)
+	if dir == null:
+		return []
+
+	var directories := []
+	dir.list_dir_begin()
+	var name = dir.get_next()
+	while name != "":
+		if dir.current_is_dir():
+			directories.append(name)
+		name = dir.get_next()
+	dir.list_dir_end()
+
+	return directories
+
+func _on_add_dialogue_button_pressed() -> void:
+	var dir := DirAccess.open(dialogue_path)
+	if dir == null:
+		return
+
+	# todo: add validation
+	var new_dialogue_name = $HUD/VBoxContainer/DialogueName.text
+	dir.make_dir(new_dialogue_name)
+
+	refresh_ui()
